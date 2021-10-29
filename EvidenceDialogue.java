f19d7c551699cf44f52158b517fa2d2bea5fe941
haydaboah <90648624+haydaboah@users.noreply.github.com>
2021-10-29 13:13:45 -0400

    Update EvidenceDialogue.java

--- EvidenceDialogue.java
+++ EvidenceDialogue.java
@@ -11,14 +11,17 @@ import java.util.Random;
 public class EvidenceDialogue {
 
     private Random rand;
+    private Player player;
     
-    public EvidenceDialogue() {
+    public EvidenceDialogue(Player player) {
+        this.player = player;
         rand = new Random();
     }
 
     //********************IF INSIDE THE CRIME ROOM (FIRST EVIDENCE)********************
         public String crimeAntiqueWatch(boolean isThere) {
-            if (isThere == true) {                                                                                                          //Hayden Addition
+            if (isThere == true) {
+                player.setFirstEvidence(true);                                                                                                          //Hayden Addition
                 String description = "You find a smashed pocket watch peeking out from beneath a table. It looks like it was thrown there.\nANTIQUE WATCH FOUND";
                 return description;
             } else {
@@ -28,7 +31,8 @@ public class EvidenceDialogue {
         }
 
         public String crimeEmptyBillfold(boolean isThere) {
-            if (isThere == true) {                                                                                                              //Hayden Addition
+            if (isThere == true) {   
+                player.setFirstEvidence(true);                                                                                                              //Hayden Addition
                 String description = "A wallet is wide open on the floor at the edge of the room. There\'s nothing inside it, not even cobwebs.\nEMPTY BILLFOLD FOUND";
                 return description;
             } else {
@@ -38,7 +42,8 @@ public class EvidenceDialogue {
         }
 
         public String crimeStrayHair(boolean isThere) {
-            if (isThere == true) {                                                                                                                                                               //Hayden Addition
+            if (isThere == true) {   
+                player.setFirstEvidence(true);                                                                                                                                                               //Hayden Addition
                 String description = "When you inspect the victim more closely, you notice a single strand of hair clinging to their shirt. It is long and clearly doesn\'t belong to the victim.\nSTRAY HAIR FOUND";
                 return description;
             } else {
@@ -48,7 +53,8 @@ public class EvidenceDialogue {
         }
 
         public String crimeHipFlask(boolean isThere) {
-            if (isThere == true) {                                                                                                                                                  //Hayden Addition
+            if (isThere == true) {  
+                player.setFirstEvidence(true);                                                                                                                                                   //Hayden Addition
                 String description = "A small, metal drinking flask sits open on the bedside table. It stinks of liquor. The coroner assures you that the victim wasn\'t drinking\nHIP FLASK FOUND";
                 return description;
             } else {
@@ -58,7 +64,8 @@ public class EvidenceDialogue {
         }
 
         public String crimeCigar(boolean isThere) {
-            if (isThere == true) {                                                                                                                                                                                                         //Hayden Addition
+            if (isThere == true) {   
+                player.setFirstEvidence(true);                                                                                                                                                                                                         //Hayden Addition
                 String description = "There are scattered ashes around the victim and near the window. It appears that someone paced the room while smoking and, if you sniff, you can almost make out the scent of tobacco still on the air.\nCIGAR FOUND";
                 return description;
             } else {
@@ -68,7 +75,8 @@ public class EvidenceDialogue {
         }
 
         public String crimeRoomKey(boolean isThere) {
-            if (isThere == true) {                                                                                                                                                                             //Hayden Addition
+            if (isThere == true) {   
+                player.setFirstEvidence(true);                                                                                                                                                                             //Hayden Addition
                 String description = "The door swings easily on its hinges and the knob turns smoothly. You deduce there are no signs of forced entry. Whoever is responsible must have had a key to the lock.\nROOM KEY FOUND";
                 return description;
             } else {
@@ -80,27 +88,32 @@ public class EvidenceDialogue {
     //********************IF INSIDE PUBLIC ROOM (SECOND EVIDENCE)********************
 
         public String pubAntiqueWatch() {
-            String description = "An old pocketwatch catches your eye with a wink of light. It\'s ornamented cover is made of well-polished silver.";
+            player.setSecondEvidence(true);
+            String description = "An old pocketwatch catches your eye with a wink of light. It\'s ornamented cover is made of well-polished silver.\nANTIQUE WATCH FOUND";
             return description;
         }
 
         public String pubEmptyBillfold() {
-            String description = "A discarded wallet is on the floor. There\'s nothing inside it, not even cobwebs.";
+            player.setSecondEvidence(true);
+            String description = "A discarded wallet is on the floor. There\'s nothing inside it, not even cobwebs.\nEMPTY BILLFOLD FOUND";
             return description;
         }
 
         public String pubStrayHair() {
-            String description = "By a trick of the light you happen to notice a single strand of hair. It is quite long, and shines like a spider\'s string on a damp morning.";
+            player.setSecondEvidence(true);
+            String description = "By a trick of the light you happen to notice a single strand of hair. It is quite long, and shines like a spider\'s string on a damp morning.\nSTRAY HAIR FOUND";
             return description;
         }
 
         public String pubHipFlask() {
-            String description = "Here you find a discarded hipflask, a neccessary item for discrete drinkers on the go.";
+            player.setSecondEvidence(true);
+            String description = "Here you find a discarded hipflask, a neccessary item for discrete drinkers on the go.\nHIP FLASK FOUND";
             return description;
         }
 
         public String pubCigar() {
-            String description = "Something smells like smoke, but all the ash trays have been recently cleaned. You notice black specs on the floor beside a once luxuriant carpet. When you lift it, you see a trampled cigar butt beneath. Singe marks testify how narrowly this place came to a fire.";
+            player.setSecondEvidence(true);
+            String description = "Something smells like smoke, but all the ash trays have been recently cleaned. You notice black specs on the floor beside a once luxuriant carpet. When you lift it, you see a trampled cigar butt beneath. Singe marks testify how narrowly this place came to a fire.\nCIGAR FOUND";
             return description;
         }
 
@@ -108,50 +121,55 @@ public class EvidenceDialogue {
         
         public String privAntiqueWatch(boolean isThere) {
             if (isThere == true) {
-                String description = "";
+                player.setThirdEvidence(true);
+                String description = "\nANTIQUE WATCH FOUND";
                 return description;
             } else {
-                String notThere = "";
+                String notThere = "Nothing to fucking return yet";
                 return notThere;
             }
         }
 
         public String privEmptyBillfold(boolean isThere) {
             if (isThere == true) {
-                String description = "";
+                player.setThirdEvidence(true);
+                String description = "\nEMPTY BILLFOLD FOUND";
                 return description;
             } else {
-                String notThere = "";
+                String notThere = "This is all because of you Winton";
                 return notThere;
             }
         }
 
         public String privStrayHair(boolean isThere) {
             if (isThere == true) {
-                String description = "";
+                player.setThirdEvidence(true);
+                String description = "\nSTRAY HAIR FOUND";
                 return description;
             } else {
-                String notThere = "";
+                String notThere = "Are you seriously this selfish?";
                 return notThere;
             }
         }
 
         public String privHipFlask(boolean isThere) {
             if (isThere == true) {
-                String description = "";
+                player.setThirdEvidence(true);
+                String description = "\nHIPFLASK FOUND";
                 return description;
             } else {
-                String notThere = "";
+                String notThere = "Never should've trusted a fucking English major";
                 return notThere;
             }
         }
 
         public String privCigar(boolean isThere) {
             if (isThere == true) {
-                String description = "";
+                player.setThirdEvidence(true);
+                String description = "\nCIGAR FOUND";
                 return description;
             } else {
-                String notThere = "";
+                String notThere = "This is fucking bullshit";
                 return notThere;
             }
         }
