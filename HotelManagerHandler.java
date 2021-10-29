8e345c27ca7dd7caab5c738491267e9e382666a4
haydaboah <90648624+haydaboah@users.noreply.github.com>
2021-10-29 13:02:04 -0400

    Update HotelManagerHandler.java

--- HotelManagerHandler.java
+++ HotelManagerHandler.java
@@ -6,22 +6,38 @@ public class HotelManagerHandler {
     private HotelManager hm;
     private int choice;
     private NPC[] characters;
+    private Player player;
+    private NPC suspect;
+    private boolean intro;
 
-    public HotelManagerHandler(NPC[] characters) {
+    public HotelManagerHandler(NPC[] characters, Player player, NPC suspect) {
         scan = new Scanner(System.in);
         this.characters = characters;
+        this.player = player;
+        this.suspect = suspect;
         hm = new HotelManager();
         choice = 0;
+        intro = true;
     }
 
-    public void initiate() {
-        System.out.println();
-        System.out.println(hm.firstEncounter("ENTER MURDER ROOM HERE"));
-        System.out.println();
-        //TimeUnit.SECONDS.sleep(1); //Wait one second
-        System.out.println(hm.greeting());
-        System.out.println();
-        level1(true);
+    public void initiate() { //if the player has found all 3 evidences
+        if ((player.getFirstEvidence() == true) && (player.getSecondEvidence() == true) && (player.getThirdEvidence() == true)) {
+            System.out.println();
+            endGame(true);
+
+        } else { //regular HM interaction
+            System.out.println();
+            if (intro == true) {
+                System.out.println(hm.firstEncounter("ENTER MURDER ROOM HERE"));
+                intro = false;
+                System.out.println();
+            }
+            System.out.println();
+            //TimeUnit.SECONDS.sleep(1); //Wait one second
+            System.out.println(hm.greeting());
+            System.out.println();
+            level1(true);
+        }
     }
 
     //First level of the Hotel Manager dialogue tree
@@ -151,4 +167,128 @@ public class HotelManagerHandler {
                 System.out.println("I\'m afraid I don\'t know what that is.");
         }
     }
+
+    public void endGame(Boolean thisLevel) {
+        while (thisLevel == true) {
+            choice = 0;
+            System.out.println("(1) Inquire about crime\n(2) Tell me about the other people here\n(3) Does this look familiar to you?\n(4) MAKE ACCUSATION \n(5) Go back\n");
+            System.out.println();
+            choice = scan.nextInt();
+            System.out.println();
+
+                    //call inquireCrime()
+                    if(choice == 1) {
+                        System.out.println(hm.inquireCrime("ENTER MURDER ROOM HERE"));
+                        System.out.println();
+                    }
+
+                    //call otherPeople()
+                    if(choice == 2) {
+                        System.out.println(hm.otherPeople());
+                        level2(true); //Jump to level 2
+                        thisLevel = false; //must reset every iteration
+                    }
+
+                    //call lookFamiliar (level4) evidence loop
+                    if(choice == 3) {
+                        level4(true);
+                    }
+
+                    if (choice == 4) {
+                        makeAccusation(true);
+                    }
+
+                    //exit endGame
+                    if(choice == 5) {
+                        thisLevel = false;
+                    }
+
+            if (player.getGameOver() == true)
+                break;
+        }
+    }
+
+    public void makeAccusation(Boolean thisLevel) {
+        System.out.println();
+        System.out.println("Oh! Have you discovered something Detective?");
+        while (thisLevel == true) {
+            choice = 0;
+            System.out.println();
+            System.out.println("(1) Chef\n(2) Maid\n(3) Senator\n(4) Imperialist\n(5) Dancer\n(6) Goon\n(7) Go back");
+            System.out.println();
+            choice = scan.nextInt();
+            System.out.println();
+
+            if (choice != 7) {
+                System.out.println(hm.makeAccusation(characters[choice-1].getCharName()));
+                System.out.println();
+                finalDecision(choice);
+                thisLevel = false;
+            } else {
+                thisLevel = false;
+            }
+
+            if (player.getGameOver() == true)
+                break;
+        }
+    }
+
+    public void finalDecision(int choice) {
+        switch (choice) {
+            case 1:    //chef
+                System.out.println(hm.postAccusation(suspect.equals(characters[0])));
+                if (suspect.equals(characters[0]) == true) {
+                    player.setCorrectAccusation(true);
+                    player.setGameOver(true);
+                } else {
+                    player.setGameOver(true);
+                }
+                break;
+            case 2:     //maid
+                System.out.println(hm.postAccusation(suspect.equals(characters[1])));
+                if (suspect.equals(characters[1]) == true) {
+                    player.setCorrectAccusation(true);
+                    player.setGameOver(true);
+                } else {
+                    player.setGameOver(true);
+                }
+                break;
+            case 3:     //sen
+                System.out.println(hm.postAccusation(suspect.equals(characters[2])));
+                if (suspect.equals(characters[2]) == true) {
+                    player.setCorrectAccusation(true);
+                    player.setGameOver(true);
+                } else {
+                    player.setGameOver(true);
+                }
+                break;
+            case 4:     //imp
+                System.out.println(hm.postAccusation(suspect.equals(characters[3])));
+                if (suspect.equals(characters[3]) == true) {
+                    player.setCorrectAccusation(true);
+                    player.setGameOver(true);
+                } else {
+                    player.setGameOver(true);
+                }
+                break;
+            case 5:     //dan
+                System.out.println(hm.postAccusation(suspect.equals(characters[4])));
+                if (suspect.equals(characters[4]) == true) {
+                    player.setCorrectAccusation(true);
+                    player.setGameOver(true);
+                } else {
+                    player.setGameOver(true);
+                }
+                break;
+            case 6:     //goon
+                System.out.println(hm.postAccusation(suspect.equals(characters[5])));
+                if (suspect.equals(characters[5]) == true) {
+                    player.setCorrectAccusation(true);
+                    player.setGameOver(true);
+                } else {
+                    player.setGameOver(true);
+                }
+                break;
+        }
+    }  
 }
